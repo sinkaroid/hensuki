a0213f05f111d8e09ff78e32b4d6ff7363ba7b74
sinkaroid <bibsindra@gmail.com>
2019-08-10 03:14:43 +0700

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# h2rwinsh
+bash https://hentai2read.com downloader and using cwget binary for windows
+
+Get started
+----
+put /bin in your local bin
+
+     $ mv  -v ~/bin* ~/usr/local/bin/rubbishword  
+
+## usage
+![](https://1.bp.blogspot.com/-Rv5kzibe_o8/XU3SRZvCD_I/AAAAAAAAJoI/-DIkfJp1prkjUw_ImmFNfnziKVmN3ML8QCLcBGAs/s1600/Screenshot_70.png)
+```sh
+$ ./hentai.sh #chmod +x if necessary
+```  
+
+```php
+# ie: hentai2read.com/lewd_inuyamasenpai/1/ | just type "lewd_inuyamasenpai/1/"
+```  
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ hentai.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#sin
+
+RED=$(tput setaf 1) 
+GREEN=$(tput setaf 2) 
+CYAN=$(tput setaf 6) 
+WHITE=$(tput setaf 7) 
+begin=$(date +"%s")
+echo -e "${RED} "
+echo "  Sin . (bash hentai2read.com downloader)"
+echo "${GREEN}"
+echo "usage : series/chapter"
+echo "ie: https://hentai2read.com/lewd_inuyamasenpai/1/"
+echo "just type: ${CYAN}lewd_inuyamasenpai/1/"
+#get
+read -p "${WHITE}PATH: " kode
+echo -e "\n"
+read -p "${WHITE}${kode} Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1 
+echo -e "reading source for /${GREEN}$kode ..
+${WHITE}"
+
+wget -O meki.html https://hentai2read.com/${kode};
+
+#do
+mkdir -pv "${kode}" 
+cat meki.html| grep "'images'" | sed 's/","/\nstatic.hentaicdn.com\/hentai/g' | sed "s/\\\//g" | sed 's/"],//g' > ${kode}/asu.txt #dog
+cd ${kode};
+wget -i asu.txt # --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36" if necessary
+cd -
+rm meki.html;
+
+#length
+termin=$(date +"%s")
+difftimelps=$(($termin-$begin))
+echo "${WHITE}$(($difftimelps / 60)) minutes and $(($difftimelps % 60)) sec. elapsed ${RED}hentai2read stealer"
+
+# damashi_ai/1/
