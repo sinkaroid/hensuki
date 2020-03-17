9ece0255106cb4f1984bfeae3ccdbbc509617ded
sinkaroid <bibsindra@gmail.com>
2020-03-18 03:32:58 +0700

    1.5 // added hnexus and implemented pattern

---
+++ bin/hnexus.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#sin
+
+RED=$(tput setaf 1) 
+GREEN=$(tput setaf 2) 
+CYAN=$(tput setaf 6) 
+WHITE=$(tput setaf 7) 
+begin=$(date +"%s")
+echo -e "${RED} "
+echo "  Sin . (Hensuki [bash hentainexus downloader])"
+echo "${WHITE}"
+echo "parameter : ['view/code']"
+echo "ie: https://hentainexus.com/view/7390"
+echo "just type: ${CYAN}7390"
+#get
+echo "code:"
+read -r kode
+echo -e "reading /${GREEN}$kode ..
+${WHITE}"
+wget -q -nv -O nex.html https://hentainexus.com/read/${kode};
+anjing=$(cat nex.html | grep -Po '1, "\K.*?(?= ::)')
+echo ${GREEN}${anjing}${WHITE}
+mkdir -pv "${anjing}"
+cat nex.html | grep -Po '"https:\K.*?(?=")' | sed 's/\\\/\\\//https:\/\//' | sed 's/\\//g' > "${anjing}"/link.txt | rm nex.html;
+cd "${anjing}";
+wget -q -nv -i link.txt | rm link.txt;
+
+#length
+termin=$(date +"%s")
+difftimelps=$(($termin-$begin))
+echo "${WHITE}$(($difftimelps / 60)) minutes and $(($difftimelps % 60)) sec. elapsed ${RED}hentainexus bash"
+
+# cat dog.txt | grep -Po '"https\K.*?(?=")' // link
+# cat dog.txt | grep -Po '"https:\K.*?(?=")' | sed 's/\\/dog/' // char "\"
