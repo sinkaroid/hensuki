3580eafd78a53466defd0992631e297e51a70b28
sinkaroid <bibsindra@gmail.com>
2020-06-12 05:04:24 +0700

    pattern update //latest nhen structure

--- bin/nhensuki.sh
+++ bin/nhensuki.sh
@@ -10,8 +10,7 @@ WHITE=$(tput setaf 7)
 
 begin=$(date +"%s")
 
-
-echo "![Nhentai stealer]!"
+echo "![Nhentai stealer]! - with mirror"
 
 #get
 echo "code:"
@@ -19,7 +18,7 @@ read -r kode
 echo -e "reading /${GREEN}$kode ..
 ${WHITE}"
 
-wget -q -nv -O ${kode}.html https://nhentai.net/g/${kode};
+wget -q -nv -O ${kode}.html https://hensuki.sinxdr.workers.dev/g/${kode};
 json="cat ${kode}.html"
 jamet="cat links.txt"
 
@@ -27,10 +26,8 @@ var="$($json | grep -oP '(?<=<span class="pretty">)[^<]*' | sed -z 's/\n//g' | s
 tag="$($json | grep -Po '(?<=<span class="name">)([^<]*)')" ## tags
 count="$($json | grep -Po '(?<=<span class="name">)[0-9]([^<]*)')" ## count
 
-
 hapus="rm ${kode}.html"
 iki="rm links.txt dog.txt"
-sniff="rm *.svg"
 
 echo "Title:" ${GREEN}$var
 echo "${WHITE}Tags:" ${RED}$tag
@@ -43,21 +40,15 @@ echo -e "still doing /${GREEN}$kode ..."
 
 #do
 
-${json} | grep img | grep -Po '<img src="\K.*?(?=")' | sed 's/\?.*//' | sed 's/t.nhentai/i.nhentai/' | sed 's/t.jpg/.jpg/' | sed 's/t.png/.png/' > "${var}"/links.txt | ${hapus};
+${json} | grep img | grep -Po '<img src="\K.*?(?=")' | sed 's/\?.*//' | sed 's/t.nhentai.net/hell.sinxdr.workers.dev/' | sed 's/t.jpg/.jpg/' | sed 's/t.png/.png/' | sed 's/.*.svg//' > "${var}"/links.txt | ${hapus};
 cd "${var}";
 wget -q -nv -i links.txt # option -i $argv if necessary show dat process
 ${jamet} | sed 's/.*thumb.jpg//' | sed 's/.*cover.jpg//' | sed 's/.*.svg//' | sed -r '/^\s*$/d' > dog.txt
-##for img in *.png; do
-   ## filename=${img%.*}
-    ##convert "$filename.png" "$filename.jpg" && rm "$img"; ## hell
-##done
+
 ParDir="$(cd ../ && pwd)/galer.sh"
 
 source $ParDir
-${sniff};
 ${iki};
-
-
 
 #length
 termin=$(date +"%s")
