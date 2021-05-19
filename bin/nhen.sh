a62491dc3ce8d94a811bb1a01bc80d43f42b3438
Curstantine <resistlyfe@gmail.com>
2021-05-19 21:00:01 +0530

    intial

--- bin/nhen.sh
+++ bin/nhen.sh
@@ -9,19 +9,20 @@ WHITE=$(tput setaf 7)
 
 #awalan
 begin=$(date +"%s")
-echo "Nhentai stealer"
+echo
+echo "${WHITE}nHentai stealer started!"
 
 # get
-echo "code:"
-read -r kode
-echo -e "reading /${GREEN}$kode ..
+echo -n "${WHITE}code: " # Makes echo and read inline
+read -r kode 
+echo -e "reading ${GREEN}/g/$kode...
 ${WHITE}"
 
 # do
 wget -q -nv -O ${kode}.html https://nhentai.net/g/${kode};
 json="cat ${kode}.html"
 jamet="cat links.txt"
-var="$($json | grep -oP '(?<=<span class="pretty">)[^<]*' | sed -z 's/\n//g' | sed 's/\//_/')" ## title > sed fg/o > _
+var="$($json | grep -oP '(?<=<span class="pretty">)[^<]*' | head -1 | sed -z 's/\n//g' | sed 's/\//_/')" ## 1st Title is shown | title > sed fg/o > _ 
 tag="$($json | grep -Po '(?<=<span class="name">)([^<]*)')" ## tags
 count="$($json | grep -Po '(?<=<span class="name">)[0-9]([^<]*)')" ## count
 
@@ -31,12 +32,12 @@ iki="rm links.txt dog.txt"
 sniff="rm *.svg"
 
 # proses
-echo "Title:" ${GREEN}$var
+echo "${WHITE}Title:" ${GREEN}$var
 echo "${WHITE}Tags:" ${RED}$tag
 echo "${WHITE}Count:" ${CYAN}$count
 echo ${WHITE}
 mkdir -pv "../result/[nhentai.net] - ${var}" 
-echo -e "Processing, pls wait /${GREEN}$kode .."
+echo -e "Please wait! Processing ${GREEN}/g/$kode..."
 ${json} | grep img | grep -Po '<img src="\K.*?(?=")' | sed 's/\?.*//' | sed 's/t.nhentai/i.nhentai/' | sed 's/t.jpg/.jpg/' | sed 's/t.png/.png/' | sed 's/t.gif/.gif/' > "../result/[nhentai.net] - ${var}"/links.txt | ${hapus};
 cd "../result/[nhentai.net] - ${var}";
 
@@ -49,4 +50,4 @@ ${iki};
 # akhiran
 termin=$(date +"%s")
 difftimelps=$(($termin-$begin))
-echo "${WHITE}$(($difftimelps / 60)) minutes and $(($difftimelps % 60)) sec. elapsed for ${GREEN}$var ${RED}/nhentai stealer"
+echo "${WHITE}$(($difftimelps / 60)) minutes and $(($difftimelps % 60)) seconds elapsed to finish ${GREEN}$var!"
